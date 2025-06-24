0ad0ae6d8326da47d0745183f75cdd581ba8e228
Edanu <Edanu@BURAHAN>
2025-06-24 16:56:01 +0300

    Add project files.

---
+++ otobusbiletotomasyonu/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otobusbiletotomasyonu
+{
+    class DB
+    {
+        public static SqlConnection baglanti;
+        //Baglanti adında bir bağlantı oluşturdum
+
+        public static bool baglanti_kontrol()
+        {
+            try
+            {
+                baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=otobusbiletsatisvt;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                return true;
+                //Veritabanına bağlanırsa baglanti_kontrol fonksiyonu "true" değeri gönderecek
+            }
+
+            catch (Exception)
+            {
+                return false;
+                //Veritabanına bağlanamazsa "false" değeri dönecek
+            }
+        }
+    }
+}
